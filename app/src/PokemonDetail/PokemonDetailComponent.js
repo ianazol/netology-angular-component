2df2c1c5da7f7642002038ef7a9a8102ba83b531
Iana <yana.kolchanova@gmail.com>
2016-12-26 16:38:40 +0300

    initial

---
+++ app/src/PokemonDetail/PokemonDetailComponent.js
@@ -0,0 +1,50 @@
+'use strict';
+
+pokemonApp.component('pokemonDetail', {
+
+    controller: function PokemonListCtrl($routeParams, PokemonsService) {
+
+        var ctrl = this;
+
+        ctrl.pokemonLoaded = false;
+
+        ctrl.pokemon = PokemonsService.get({
+            pokemonId: $routeParams['pokemonId']
+        }, function(successResult) {
+            // Окей!
+            ctrl.notfoundError = false;
+            ctrl.pokemonLoaded = true;
+
+            ctrl.activeTab = 1;
+            ctrl.disableControlTab = true;
+        }, function(errorResult) {
+            // Не окей..
+            ctrl.notfoundError = true;
+            ctrl.pokemonLoaded = true;
+
+
+        });
+
+        ctrl.pokemon.$promise.then(function(result) {
+            //$scope.pokemonLoaded = true;
+        });
+
+        ctrl.deletePokemon = function(pokemonId) {
+
+            ctrl.pokemon.$delete({
+                pokemonId: pokemonId
+            }, function(successResult) {
+                // Окей!
+                ctrl.deletionSuccess = true;
+            }, function(errorResult) {
+                // Не окей..
+                ctrl.deletionError = true;
+            });
+
+        }
+
+    },
+
+    templateUrl: './src/PokemonDetail/PokemonDetail.html'
+
+});
